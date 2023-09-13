e0d90b081a60eb00551f35e7317f9d97405cce59
albertthomas2205 <122583969+albertthomas2205@users.noreply.github.com>
2023-09-13 16:19:08 +0530

    settings

--- shoerack/shoerack/settings.py
+++ shoerack/shoerack/settings.py
@@ -114,10 +114,10 @@ DATABASES = {
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
